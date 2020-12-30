8888988b38051422018dff9f3486ff021cc75f5f
Archurrito <artolecuona15@gmail.com>
2020-12-30 01:57:25 -0400

    yarn foundation-sites

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'foundation-sites'
+require("jquery")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+Foundation.addToJquery($);
 
 $(function(){ $(document).foundation(); });
