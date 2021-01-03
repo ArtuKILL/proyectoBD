bd119c0edee16b90a6e7ac07f978cef3bbd1ba21
Archurrito <artolecuona15@gmail.com>
2021-01-03 13:00:39 -0400

    Se añade las clases de prototipos de foundation y muestra los atributos del Cliente en el root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :clientes
   get 'home/index'
   get 'test/index'
-  root 'test#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
