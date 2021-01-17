27b87cf865c85ec3ae11d3c6ae7b93e36b014e85
ilixna <47933363+ilixna@users.noreply.github.com>
2021-01-17 02:24:06 -0400

    Agregue tablas que no necesitan el scaffold en la bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_174317) do
+ActiveRecord::Schema.define(version: 2021_01_12_175106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "a_p_hist", primary_key: "fecha_inicial", id: :date, force: :cascade do |t|
+    t.date "fecha_final"
+  end
+
+  create_table "agencias_rallies", id: false, force: :cascade do |t|
+    t.integer "cupos", limit: 2
+  end
 
   create_table "agencies", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -23,13 +31,50 @@ ActiveRecord::Schema.define(version: 2021_01_07_174317) do
     t.string "url", limit: 100, null: false
     t.string "alcance", limit: 15, null: false
     t.text "descripcion", null: false
-    t.string "tipo", limit: 25, null: false
+    t.string "tipo", limit: 20, null: false
+    t.check_constraint "(alcance)::text = ANY ((ARRAY['Local'::character varying, 'Nacional'::character varying, 'Internacional'::character varying])::text[])", name: "alcance_a"
+    t.check_constraint "(tipo)::text = ANY ((ARRAY['Tour Operadoras'::character varying, 'Mayoristas'::character varying, 'Minoristas'::character varying, 'Mixta'::character varying])::text[])", name: "tipo_a"
+  end
+
+  create_table "alojamientos_hoteles", primary_key: "id_hotel", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_hotel", null: false
+  end
+
+  create_table "area_intereses", primary_key: "id_interes", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "categoria_interes", null: false
+    t.text "descripcion_interes"
+  end
+
+  create_table "areaint_es", primary_key: "id_areaint_esp", id: :bigint, default: nil, force: :cascade do |t|
+    t.text "comentario"
+  end
+
+  create_table "asesores", primary_key: "id_asesor", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_asesor", null: false
+    t.string "seg_nombre_asesor"
+    t.string "apellido_asesor", null: false
+    t.string "seg_apellido_asesor", null: false
+  end
+
+  create_table "atracciones_circuitos", id: false, force: :cascade do |t|
   end
 
   create_table "bancos", force: :cascade do |t|
     t.string "nombre_banco"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "calendarios_anuales", primary_key: "fecha_salida", id: :date, force: :cascade do |t|
+    t.text "descripcion"
+  end
+
+  create_table "circuitos", primary_key: "orden", id: :string, force: :cascade do |t|
+    t.string "max_dias", null: false
+  end
+
+  create_table "ciudades_localidades", primary_key: "id_ciudad", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_ciudad", null: false
   end
 
   create_table "clientes", force: :cascade do |t|
@@ -53,10 +98,45 @@ ActiveRecord::Schema.define(version: 2021_01_07_174317) do
     t.index ["reset_password_token"], name: "index_clientes_on_reset_password_token", unique: true
   end
 
+  create_table "detalles_servicios", primary_key: "id_servicio", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "tipo", null: false
+    t.text "descripcion", null: false
+    t.string "comida"
+  end
+
+  create_table "empresa_prooveedor", primary_key: "id_proveedor", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_proveedor", limit: 40, null: false
+    t.string "tipo_proveedor", null: false
+  end
+
   create_table "forma_pagos", force: :cascade do |t|
     t.string "tipo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hist_precios", primary_key: "fecha_inicio", id: :date, force: :cascade do |t|
+    t.date "fecha_final"
+    t.integer "precio_base", null: false
+  end
+
+  create_table "itinerarios_atracciones", id: false, force: :cascade do |t|
+    t.integer "orden", limit: 2
+  end
+
+  create_table "ofertas", primary_key: "id_oferta", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_oferta", limit: 40, null: false
+    t.integer "porcentaje", limit: 2, null: false
+    t.string "tipo_persona", limit: 20, null: false
+    t.date "fecha_inicial", null: false
+    t.date "fecha_final"
+  end
+
+  create_table "paises", primary_key: "id_pais", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_pais", null: false
+    t.string "continente", limit: 10, null: false
+    t.string "region", null: false
+    t.string "nacionalidad", limit: 20, null: false
   end
 
   create_table "paquete_contratos", force: :cascade do |t|
@@ -70,6 +150,15 @@ ActiveRecord::Schema.define(version: 2021_01_07_174317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "pasaportes", primary_key: "num_pasaporte", id: :string, force: :cascade do |t|
+  end
+
+  create_table "premios", primary_key: "id_premio", id: :bigint, default: nil, force: :cascade do |t|
+    t.string "nombre_premio", null: false
+    t.string "puesto", null: false
+    t.text "descripcion", null: false
+  end
+
   create_table "registro_clientes", force: :cascade do |t|
     t.integer "nro_registro"
     t.date "fecha_registro"
@@ -77,11 +166,21 @@ ActiveRecord::Schema.define(version: 2021_01_07_174317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "servicios_hoteles", id: false, force: :cascade do |t|
+  end
+
+  create_table "socios", primary_key: "fecha_inicial", id: :date, force: :cascade do |t|
+    t.date "fecha_final"
+  end
+
   create_table "tipos", force: :cascade do |t|
     t.string "numero"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "viajes_compras", id: false, force: :cascade do |t|
   end
 
 end
