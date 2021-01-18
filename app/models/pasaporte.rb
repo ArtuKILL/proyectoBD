ddcf12b3fc81d891e66d4b20030ae15dc50b69ec
Archurrito <Artolecuona15@gmail.com>
2021-01-18 16:58:19 -0400

    pasaporte creado

---
+++ app/models/pasaporte.rb
@@ -0,0 +1,2 @@
+class Pasaporte < ApplicationRecord
+end
---
+++ test/models/pasaporte_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PasaporteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
