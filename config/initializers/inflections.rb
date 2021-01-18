deb4e8f8303d82b6ab71e0c599b9bf68d4c59ff2
Archurrito <Artolecuona15@gmail.com>
2021-01-18 16:07:20 -0400

    alojamiento_hotel creado

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -39,5 +39,5 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.singular /([djlnrs])es([A-Z]|_|$)/, '\1\2'
   inflect.singular /(.*)ces([A-Z]|_|$)$/i, '\1z\2'
 
-  inflect.irregular('el', 'los')
+  #inflect.irregular('el', 'los')
 end
--- db/migrate/20210118195827_create_detalles_servicio.rb
+++
@@ -1,8 +0,0 @@
-class CreateDetallesServicio < ActiveRecord::Migration[6.1]
-  def change
-    create_table :detalles_servicio do |t|
-
-      t.timestamps
-    end
-  end
-end
