18f9dbc226795721470b6035f8c1e2bb849db89a
Archurrito <Artolecuona15@gmail.com>
2021-01-20 04:28:56 -0400

    Atraccion/Valoracion relacion

--- app/models/atraccion.rb
+++ app/models/atraccion.rb
@@ -1,6 +1,8 @@
 class Atraccion < ApplicationRecord
     has_many :areaint_es
-    belongs_to :ciudad_localidad
     has_many :atraccion_circuitos
+    has_many :valoraciones
     has_many :circuitos, :through => :atraccion_circuitos
+
+    belongs_to :ciudad_localidad
 end
--- app/models/valoracion.rb
+++ app/models/valoracion.rb
@@ -2,4 +2,5 @@ class Valoracion < ApplicationRecord
     belongs_to :rally
     belongs_to :ciudad_localidad
     belongs_to :pais
+    belongs_to :atraccion
 end
