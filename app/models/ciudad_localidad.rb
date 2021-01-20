fc3f5c674ef3f4ad599afd66e301140011b890ab
Archurrito <Artolecuona15@gmail.com>
2021-01-20 04:26:33 -0400

    Ciudad_localidad/Valoracion relacion

--- app/models/ciudad_localidad.rb
+++ app/models/ciudad_localidad.rb
@@ -2,5 +2,6 @@ class CiudadLocalidad < ApplicationRecord
     self.table_name = "#{self.table_name_prefix}ciudades_localidades"
     has_many :atracciones
     has_many :circuitos
+    has_many :valoraciones
     belongs_to :pais
 end
--- app/models/valoracion.rb
+++ app/models/valoracion.rb
@@ -1,3 +1,4 @@
 class Valoracion < ApplicationRecord
     belongs_to :rally
+    belongs_to :ciudad_localidad
 end
