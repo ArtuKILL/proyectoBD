1c271d7692474d08c68a834fbe524ee7fe299391
Carlos Hernandez <soycarlosluis862@gmail.com>
2021-01-20 01:10:05 -0400

    socios update

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,5 +1,6 @@
-class Agency < ApplicationRecord
-    has_many :socios ,:class_name => 'Socio' ,foreign_key: "agencia1_id"
+class Agency < ApplicationRecordexit
+    has_many :socio1, class_name: "Socio", foreign_key: "agencia1_id"
+    has_many :socio2, class_name: "Socio", foreign_key: "agencia2_id"
     has_many :paquetes
     has_many :a_p_hists
     has_many :empresa_proveedores, :through => :a_p_hists
