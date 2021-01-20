ef9fad972850b8f946a7aa2adf44bb99d83f2d74
Archurrito <Artolecuona15@gmail.com>
2021-01-20 02:51:14 -0400

    detalle_servicio/paquete relacion

--- app/models/detalle_servicio.rb
+++ app/models/detalle_servicio.rb
@@ -1,3 +1,4 @@
 class DetalleServicio < ApplicationRecord
     self.table_name = "#{self.table_name_prefix}detalles_servicios"
+    belongs_to :paquete
 end
--- app/models/paquete.rb
+++ app/models/paquete.rb
@@ -2,4 +2,5 @@ class Paquete < ApplicationRecord
     belongs_to :agencia, foreign_key: "id"
     has_many :paquete_contratos
     has_many :hist_precios
+    has_many :detalle_servicios
 end
