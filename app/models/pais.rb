d2591a02c1346d8b844f7c3cb0c5499e1fce7ab4
Archurrito <Artolecuona15@gmail.com>
2021-01-20 04:27:35 -0400

    Pais/Valoracion relacion

--- app/models/pais.rb
+++ app/models/pais.rb
@@ -1,5 +1,6 @@
 class Pais < ApplicationRecord
     has_many :ciudad_localidades
     has_many :pasaportes
+    has_many :valoraciones
     has_many :viajeros, :through => :pasaportes
 end
--- app/models/valoracion.rb
+++ app/models/valoracion.rb
@@ -1,4 +1,5 @@
 class Valoracion < ApplicationRecord
     belongs_to :rally
     belongs_to :ciudad_localidad
+    belongs_to :pais
 end
