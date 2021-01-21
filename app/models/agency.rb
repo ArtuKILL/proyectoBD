75195acb0382a1d1de49b446c3d3e0f852d19787
Carlos Hernandez <soycarlosluis862@gmail.com>
2021-01-21 01:23:44 -0400

    a

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,7 @@
 class Agency < ApplicationRecord
+    
     has_many :socios, class_name: "Socio", foreign_key: "agencia1_id"
-    #has_many :socio2, class_name: "Socio", foreign_key: "agencia2_id"
+    has_many :socio2, class_name: "Socio", foreign_key: "agencia2_id"
     has_many :paquetes
     has_many :a_p_hists
     has_many :ofertas
