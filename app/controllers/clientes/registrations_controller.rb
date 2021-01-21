388bfe3c88436f42f30175286055e5f0164ced54
Archurrito <Artolecuona15@gmail.com>
2021-01-21 02:01:20 -0400

    Campo ID eliminado de editar cliente

--- app/controllers/clientes/registrations_controller.rb
+++ app/controllers/clientes/registrations_controller.rb
@@ -2,14 +2,14 @@
 
 class Clientes::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super do |resource|
-      logger.debug "NO SE QUE ES ESTO -> #{resource.inspect}"
-    end
-  end
+  # def new
+  #   super do |resource|
+  #     logger.debug "NO SE QUE ES ESTO -> #{resource.inspect}"
+  #   end
+  # end
 
   # POST /resource
   # def create
@@ -51,9 +51,9 @@ class Clientes::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :segundo_nombre, :primer_apellido, :segundo_apellido, :direccion, :genero, :tipo, :fecha_nac])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
