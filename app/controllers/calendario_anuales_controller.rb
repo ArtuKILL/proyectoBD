2dc81bdf31732c0ca112bfe8c3231915177c04d6
ilixna <47933363+ilixna@users.noreply.github.com>
2021-01-25 14:38:21 -0400

    calenario funcionando

--- app/controllers/calendario_anuales_controller.rb
+++ app/controllers/calendario_anuales_controller.rb
@@ -64,10 +64,14 @@ class CalendarioAnualesController < ApplicationController
     end
   end
 
+  def link_to_submit(*args, &block)
+    link_to_function (block_given? ? capture(&block) : args[0]), "$(this).closest('form').submit()", args.extract_options!
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_calendario_anual
-      @calendario_anual = CalendarioAnual.find(params[:fecha_salida])
+      @calendario_anual = CalendarioAnual.find(params[:id])
     end
 
     def set_paquete
